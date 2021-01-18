3d61177dc1269d37b102d6fb88f8eaa3dfda9560
yedidyaroy <76555587+yedidyaroy@users.noreply.github.com>
2021-01-18 20:05:23 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
 var tom,tomoneImage,tomtwoImage,tomthreeImage;
 var backGround,backGroundImage;
-var jerry,jerryImage;
+var jerry,jerryoneImage,jerrytwoImage,jerrythreeImage;
 function preload() {
     //load the images here
 backGroundImage = loadImage("garden.png")
-tomoneImage = loadImage("tomOne.png")
-jerryImage = loadAnimation("jerryOne.png")
+tomoneImage = loadAnimation("tomOne.png")
+jerryoneImage = loadAnimation("jerryOne.png")
 tomtwoImage = loadAnimation("tomTwo.png","tomThree.png")
+jerrytwoImage = loadAnimation("jerryTwo.png","jerryThree.png")
+tomthreeImage = loadAnimation("tomFour.png")
+jerrythreeImage = loadAnimation("jerryFour.png")
 }
 
 function setup(){
@@ -16,19 +19,30 @@ function setup(){
     backGround.addImage(backGroundImage);
 
     tom = createSprite(550,470,20,20);
-    tom.addImage(tomoneImage);
+    tom.addAnimation("standing",tomoneImage);
+    tom.addAnimation("moving",tomtwoImage)
+    tom.addAnimation("colliding",tomthreeImage)
     tom.scale=0.1;
+    tom.setCollider("circle",0,0,550)
 
     jerry = createSprite(100,450,20,20);
-    jerry.addAnimation("standing",jerryImage);
+    jerry.addAnimation("standing",jerryoneImage);
+    jerry.addAnimation("moving",jerrytwoImage)
+    jerry.addAnimation("colliding",jerrythreeImage)
     jerry.scale=0.1;
+    jerry.setCollider("circle",0,0,400)
 }
 
 function draw() {
 
     background("white");
     //Write condition here to evalute if tom and jerry collide
+    if (tom.isTouching(jerry)){
+        tom.collide(jerry);
+        tom.changeAnimation("colliding",tomthreeImage)
 
+        jerry.changeAnimation("colliding",jerrythreeImage)
+    }
     drawSprites();
 }
 
@@ -39,6 +53,8 @@ function keyPressed(){
   if (keyCode === LEFT_ARROW){
    tom.velocityX=-5;
    tom.changeAnimation("moving",tomtwoImage);
+
+   jerry.changeAnimation("moving",jerrytwoImage);
    //tom.changeAnimation("moving")
   }
 
